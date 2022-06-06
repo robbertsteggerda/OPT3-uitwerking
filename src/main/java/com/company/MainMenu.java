786d576d282a5922a36ee1048a362c58caeff18d
ro0b0 <77584281+ro0b0@users.noreply.github.com>
2022-06-06 23:50:21 +0200

    bug fixes

--- src/main/java/com/company/MainMenu.java
+++ src/main/java/com/company/MainMenu.java
@@ -2,16 +2,16 @@ package com.company;
 
 import java.util.Scanner;
 
-public class MainMenu {
+public class MainMenu { //als je onjuiste input geeft reageert het programma niet goed. Helaas had ik geen tijd om dit op te lossen. Zolang uw input correct is zou het programma correct moeten werken
     public static void menu() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("kies een optie");
         System.out.println("1. voer een nieuwe cliënt in");
         System.out.println("2. toon lijst met cliënten");
         System.out.println("3.bereken prijs voor een cliënt");
-        System.out.println("4 om af te sluiten)");
 
         int input = scanner.nextInt();
+        Database database = Database.getInstance();
 
         switch (input) {
             case 1:
@@ -25,34 +25,28 @@ public class MainMenu {
                 System.out.print("Voer het cliëntnummer in in: ");
                 int clientNummer = scanner.nextInt();
 
-                Database database = Database.getInstance();
+
                 database.addClient(new Client(naam, leeftijd, aantalUren, clientNummer));
                 menu();
             case 2:
-                Database database1 = Database.getInstance();
-                for (int i = 0; i < database1.getClienten().size(); i++) {
-                    Client client = database1.getClienten().get(i);
+                for (int i = 0; i < database.getClienten().size(); i++) {
+                    Client client = database.getClienten().get(i);
                     client.printInfo();
                 }
                 menu();
             case 3:
                 scanner.nextLine();
-                System.out.print("Voer het cliëntnummer in om de prijs te berkeneen: ");
+                System.out.print("Voer het cliëntnummer in om de prijs te berekenen: ");
                 int ingevoerdClientNummer = scanner.nextInt();
-                Database database2 = Database.getInstance();
-                for (int i = 0; i < database2.getClienten().size(); i++) {
-                    Client client = database2.getClienten().get(i);
+                for (int i = 0; i < database.getClienten().size(); i++) {
+                    Client client = database.getClienten().get(i);
                     if (client.getClientnummer() == ingevoerdClientNummer) {
                         IncBTWFactuur factuur = new IncBTWFactuur();
                         System.out.printf("de prijs is €%f", factuur.berekenPrijs(client));
                         System.out.println();
                         menu();
                     }
-                    System.out.println("Cliënt kon niet worden gevonden");
-                    menu();
                 }
-            case 4:
-                System.out.println("afsluiten...");
         }
     }
 }
